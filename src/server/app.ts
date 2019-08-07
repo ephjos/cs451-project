411148a024a411141416f55b80265a9694779f81
Joseph Hines <jthines3@gmail.com>
2019-08-07 14:28:16 -0400

    Added missing package, added top level build script (bash)

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,14 +1,14 @@
-import * as express from 'express';
-import * as path from 'path';
+import * as express from 'express'
+import * as path from 'path'
 //x import * as fs from "fs";
 //x import * as favicon from 'serve-favicon';
 //x import * as logger from 'morgan';
 //x import * as cookieParser from 'cookie-parser';
-import * as bodyParser from 'body-parser';
-import * as session from 'express-session';
+import * as bodyParser from 'body-parser'
+import * as session from 'express-session'
 
-const port = process.env.PORT || '5000';
-const app = express();
+const port = process.env.PORT || '5000'
+const app = express()
 
 // view engine setup
 //x app.set('views', path.join(__dirname, 'views'));
@@ -21,12 +21,22 @@ const app = express();
 //x app.use(cookieParser());
 
 app.set('port', port)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('.'));
-app.use(session({resave: true, saveUninitialized: true, secret: 'SOMERANDOMSECRETHERE', cookie: { maxAge: 60000 }}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('.'))
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'SOMERANDOMSECRETHERE',
+    cookie: { maxAge: 60000 },
+  }),
+)
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(app.get('port'), (): void => {
-    console.log(`Server running on port ${app.get('port')}`);
-});
+app.listen(
+  app.get('port'),
+  (): void => {
+    console.log(`Server running on port ${app.get('port')}`)
+  },
+)
