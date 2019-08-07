2edae3f5b5031ff133729171b81ed1f6c7dcf917
Bassey Duke <bassey.bd@gmail.com>
2019-08-07 16:10:35 -0400

    initial database stuff

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,30 +1,34 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as session from 'express-session';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as mongoose from 'mongoose' ;
+const MongoStore = require('connect-mongo')(session);
 
+mongoose.connect('mongodb://localhost:8080/my-database', {
+  useNewUrlParser: true
+});
 
-// const MongoStore = require('connect-mongo')(session);
-
+const db = mongoose.connection
 const app = express();
 const port = process.env.PORT || '8080';
 
 app.set('port', port)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(express.static('.'));
 
 app.use(session({
   resave: true,
   saveUninitialized: true,
   secret: 'GameSession',
-  cookie: { maxAge: 60000 }}));
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({ mongooseConnection: db })
+}));
 
-app.get('/', function (req, res) {
-  res.send('Server is up')
-})
+
+app.get('/api', function (req, res) {
+  res.send('This is an api')
+});
 
 
 app.listen(app.get('port'), (): void => {
