6f45f2688c8f7e3cc07cce3cb279c445a7b25ee4
Joseph Hines <jthines3@gmail.com>
2019-08-09 10:14:44 -0400

    Created Mongo Atlas DB, connection and writing sessions working

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,16 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as session from 'express-session';
-import * as mongoose from 'mongoose' ;
+import * as mongoose from 'mongoose';
+
+require('dotenv').config()
+
 const MongoStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb://localhost:8080/my-database', {
-  useNewUrlParser: true
-});
+mongoose.connect(process.env.MONGO_DB, {
+  useNewUrlParser: true,
+}).then(() => { console.log("DB Connected") })
+  .catch(() => { console.log("Could not connected to DB") });
 
 const db = mongoose.connection
 const app = express();
@@ -18,18 +22,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: 'GameSession',
-  cookie: { maxAge: 60000 },
-  store: new MongoStore({ mongooseConnection: db })
-}));
-
 app.get('/api', function (req, res) {
   res.send('This is an api')
 });
 
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.MONGO_SECRET,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({ mongooseConnection: db })
+}));
 
 app.listen(app.get('port'), (): void => {
   console.log(`Server running on port ${app.get('port')}`);
