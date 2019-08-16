1beee6093ecfc4a75dac912319494d15fb8f7685
Joseph Hines <jthines3@gmail.com>
2019-08-15 21:55:31 -0400

    Queue and PlayerPromise tests added

--- src/server/test/PlayerPromise.test.ts
+++ src/server/test/PlayerPromise.test.ts
@@ -1,4 +1,21 @@
+import PlayerPromise from '../src/PlayerPromise'
 
-test('blank', () => {
-    expect(true).toBeTruthy()
+test('copies id', () => {
+    let check = false
+    let resolve = (value: boolean) => check = value
+    let id = "an id"
+    let pp = new PlayerPromise(id, resolve)
+
+    expect(pp.id).toEqual(id)
+});
+
+test('properly calls resolve function', () => {
+    let check = false
+    let resolve = (value: boolean) => check = value
+    let id = "an id"
+    let pp = new PlayerPromise(id, resolve)
+
+    expect(check).toEqual(false)
+    pp.resolve(true)
+    expect(check).toEqual(true)
 });
--- src/server/test/Queue.test.ts
+++ src/server/test/Queue.test.ts
@@ -1,4 +1,62 @@
+import Queue from '../src/Queue'
 
-test('blank', () => {
-    expect(true).toBeTruthy()
+test('creates empty queue', () => {
+    let q = new Queue()
+
+    expect(q.peek()).toBeUndefined()
+});
+
+
+test('properly calculates length', () => {
+    let q = new Queue()
+    let l = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+    l.forEach((n) => { q.enqueue(n) })
+
+    expect(q.length()).toEqual(l.length)
+});
+
+test('properly peeks', () => {
+    let q = new Queue()
+    let l = [1, 2, 3]
+
+    l.forEach((n) => { q.enqueue(n) })
+
+    expect(q.peek()).toEqual(l[0])
+});
+
+test('properly enqueues and dequeues', () => {
+    let q = new Queue()
+    let l = [1, 2, 3]
+
+    l.forEach((n) => { q.enqueue(n) })
+
+    l.forEach((n) => {
+        expect(q.dequeue()).toEqual(n)
+    })
+});
+
+test('properly handles different types', () => {
+    let checkQueue = (l: any[]) => {
+        let q = new Queue()
+
+        l.forEach((n) => { q.enqueue(n) })
+
+        l.forEach((n) => {
+            expect(q.dequeue()).toEqual(n)
+        })
+    }
+
+    let num = [1, 2, 3, 4, 5]
+    checkQueue(num)
+    checkQueue([num, num, num, num, num])
+
+    let str = ["1", "2", "3", "4", "5"]
+    checkQueue(str)
+    checkQueue([str, str, str, str, str])
+
+    let fun = [() => { }, () => { }, () => { }, () => { }, () => { }]
+    checkQueue(fun)
+    checkQueue([fun, fun, fun, fun, fun])
+
 });
